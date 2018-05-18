41d903a26206795d2694b74357264f055712eb77
mrpeny <bence.konya@captaincode.eu>
2018-05-18 21:17:55 +0200

    feat: Add ExoPlayer to show step videos

--- app/src/main/java/eu/captaincode/bakingapp/ui/StepDetailActivity.java
+++ app/src/main/java/eu/captaincode/bakingapp/ui/StepDetailActivity.java
@@ -1,6 +1,7 @@
 package eu.captaincode.bakingapp.ui;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import eu.captaincode.bakingapp.R;
@@ -17,16 +18,20 @@ public class StepDetailActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            Recipe mRecipe = extras.getParcelable(ARG_RECIPE);
-            int mStepPosition = extras.getInt(ARG_STEP_POSITION);
+            Recipe recipe = extras.getParcelable(ARG_RECIPE);
+            int stepPosition = extras.getInt(ARG_STEP_POSITION);
+            showFragment(recipe, stepPosition);
+        }
+    }
 
-            StepDetailFragment stepDetailFragment = StepDetailFragment.newInstance(mRecipe,
-                    mStepPosition);
-            getSupportFragmentManager().beginTransaction()
+    private void showFragment(Recipe recipe, int stepPosition) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.findFragmentById(R.id.container_step_detail) == null) {
+            StepDetailFragment stepDetailFragment = StepDetailFragment.newInstance(recipe,
+                    stepPosition);
+            fragmentManager.beginTransaction()
                     .add(R.id.container_step_detail, stepDetailFragment)
                     .commit();
         }
     }
-
-
 }
--- app/src/main/java/eu/captaincode/bakingapp/ui/StepDetailFragment.java
+++ app/src/main/java/eu/captaincode/bakingapp/ui/StepDetailFragment.java
@@ -1,12 +1,30 @@
 package eu.captaincode.bakingapp.ui;
 
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import eu.captaincode.bakingapp.R;
 import eu.captaincode.bakingapp.databinding.FragmentStepDetailBinding;
@@ -16,9 +34,13 @@ import eu.captaincode.bakingapp.model.Step;
 public class StepDetailFragment extends Fragment {
     private static final String ARG_RECIPE = "recipe";
     private static final String ARG_STEP_POSITION = "step-position";
+    public static final String VIDEO_POSITION = "video-position";
 
     private Recipe mRecipe;
     private int mStepPosition;
+    private SimpleExoPlayer mPlayer;
+    private SimpleExoPlayerView mSimpleExoPlayerView;
+    private long mVideoPosition;
 
     public StepDetailFragment() {
         // Required empty public constructor
@@ -49,7 +71,63 @@ public class StepDetailFragment extends Fragment {
                 R.layout.fragment_step_detail, container, false);
         Step step = mRecipe.getSteps().get(mStepPosition);
         stepDetailBinding.setStep(step);
-
+        mSimpleExoPlayerView = stepDetailBinding.exoplayerStepDetail;
+        if (savedInstanceState != null) {
+            mVideoPosition = savedInstanceState.getLong(VIDEO_POSITION);
+        }
         return stepDetailBinding.getRoot();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        initializePlayer();
+    }
+
+    private void initializePlayer() {
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        mPlayer = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector);
+        mSimpleExoPlayerView.setPlayer(mPlayer);
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getActivity(),
+                Util.getUserAgent(getActivity(), getString(R.string.app_name)));
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+        Uri videoUri = Uri.parse(mRecipe.getSteps().get(mStepPosition).getVideoUrl());
+        MediaSource videoSource = new ExtractorMediaSource(videoUri, dataSourceFactory,
+                extractorsFactory, null, null);
+
+        mPlayer.prepare(videoSource);
+        mPlayer.setPlayWhenReady(true);
+        mPlayer.seekTo(mVideoPosition);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mPlayer.setPlayWhenReady(false);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+    private void releasePlayer() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        long currentPosition = mPlayer.getCurrentPosition();
+        outState.putLong(VIDEO_POSITION, currentPosition);
+        super.onSaveInstanceState(outState);
     }
 }
